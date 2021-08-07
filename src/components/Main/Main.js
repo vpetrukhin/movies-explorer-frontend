f1e7405d9b1a2a3dd5abbc090ebe9abf711752d8
Vasya <vpetruin@inbox.ru>
2021-08-07 04:42:36 +0300

    Сверстаны компоненты Promo и NavTab

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Promo from '../Promo/Promo';
+
 const Main = () => {
   
   return (
     <main className="main">
-      main
+      <Promo />
     </main>
   )
 }
---
+++ src/components/NavTab/NavTab.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const NavTab = () => {
+  
+  return (
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__item">
+          <a href="#" className="nav__link">О проекте</a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">Технологии</a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">Студент</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+export default NavTab;
