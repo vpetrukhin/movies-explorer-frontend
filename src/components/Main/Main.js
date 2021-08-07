37e06c81f34b5d3dfe4e1f9ee7bbe08f2114d581
Vasya <vpetruin@inbox.ru>
2021-08-07 07:18:42 +0300

    Сверстан компонент Techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
 
 const Main = () => {
   
@@ -9,6 +10,7 @@ const Main = () => {
     <main className="main">
       <Promo />
       <AboutProject />
+      <Techs />
     </main>
   )
 }
